c31057cbb123b9601a78cd9a4ab3bc60c6217814
mHat <>
2022-02-03 13:40:15 +0800

    Updated color and removed redundant info

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,7 +1,7 @@
 import { Colors } from "./types";
 
 export const baseColors = {
-  failure: "#ED4B9E",
+  failure: "#EF9A9A",
   primary: "#1FC7D4",
   primaryBright: "#53DEE9",
   primaryDark: "#0098A1",
